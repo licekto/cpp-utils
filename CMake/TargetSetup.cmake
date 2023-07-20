e9326d9a7e8e92eff999792e6e66e60fdbc29c31
Tomas Licek <licekto@gmail.com>
2023-07-20 20:04:35 +0200

    Cleanup

--- CMake/TargetSetup.cmake
+++ CMake/TargetSetup.cmake
@@ -1,11 +1,5 @@
 cmake_minimum_required(VERSION 3.21)
 
-set(LOG_LEVEL Info)
-if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(LOG_LEVEL Trace)
-endif()
-configure_file(${PROJECT_SOURCE_DIR}/Source/CppUtils/LogLevel.hpp.in ${CMAKE_BINARY_DIR}/generated/LogLevel.hpp)
-
 set(ALL_SOURCES_GLOBAL "" CACHE INTERNAL "")
 
 function(add_sources_to_list)
@@ -24,8 +18,4 @@ function(setup_target)
     target_include_directories(${ARG_TARGET} PRIVATE ${CMAKE_BINARY_DIR}/generated/)
     set_property(TARGET ${ARG_TARGET} PROPERTY CXX_STANDARD 20)
     target_link_libraries(${ARG_TARGET} Threads::Threads)
-
-    if(cpp-utils)
-        target_link_libraries(${ARG_TARGET} cpp-utils)
-    endif()
 endfunction()
